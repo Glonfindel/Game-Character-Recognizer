b06b726fabd92feebe230c6230f2fdc20899eee5
Marta Skrajda <s13904@pjwstk.edu.pl>
2017-06-17 16:00:03 +0200

    working commit

--- Game Character Recognizer/Game Character Recognizer/Form1.Designer.cs
+++ Game Character Recognizer/Game Character Recognizer/Form1.Designer.cs
@@ -29,11 +29,48 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.imageBox = new Emgu.CV.UI.ImageBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox
+            // 
+            this.imageBox.Location = new System.Drawing.Point(311, 12);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(400, 300);
+            this.imageBox.TabIndex = 2;
+            this.imageBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(183, 416);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 48);
+            this.label1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(896, 602);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.imageBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Game Character Recognizer/Game Character Recognizer/Form1.cs
+++ Game Character Recognizer/Game Character Recognizer/Form1.cs
@@ -1,20 +1,146 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System.IO;
+using Emgu.CV.Features2D;
+using Emgu.CV.Flann;
+using Emgu.CV.Util;
 
 namespace Game_Character_Recognizer
 {
     public partial class Form1 : Form
     {
+        VideoCapture camera;
+        Image<Bgr, byte> frame;
+        string[] files;
+        List<Image<Bgr, byte>> images = new List<Image<Bgr, byte>>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                camera = new VideoCapture();
+                camera.SetCaptureProperty(CapProp.FrameWidth, 400);
+                camera.SetCaptureProperty(CapProp.FrameHeight, 300);
+            }
+            catch (Exception ex)
+            {
+                Text = ex.Message;
+                return;
+            }
+
+            files = Directory.GetFiles(@".\images\", "*.jpg");
+            foreach (var file in files)
+            {
+                images.Add(new Image<Bgr, byte>(new Bitmap(file)));
+            }
+
+            Application.Idle += new EventHandler(this.Detect);
+        }
+
+        public void Detect(object sender, EventArgs arg)
+        {
+            try
+            {
+                int i = 0;
+                frame = camera.QueryFrame().ToImage<Bgr, Byte>();
+                foreach (var image in images)
+                {
+                    using (Mat modelImage = image.Mat)
+                    using (Mat observedImage = frame.Mat)
+                    {
+                        Draw(modelImage, observedImage, Path.GetFileName(files[i]));
+                    }
+                    i++;
+                }
+            }
+            catch (Exception ex)
+            {
+                Text = ex.Message;
+            }
+            imageBox.Image = frame;
+        }
+
+        static void FindMatch(Mat modelImage, Mat observedImage, out VectorOfKeyPoint modelKeyPoints, out VectorOfKeyPoint observedKeyPoints, VectorOfVectorOfDMatch matches, out Mat mask, out Mat homography)
+        {
+            int k = 2;
+            double uniquenessThreshold = 0.60;
+
+            homography = null;
+
+            modelKeyPoints = new VectorOfKeyPoint();
+            observedKeyPoints = new VectorOfKeyPoint();
+
+            using (UMat uModelImage = modelImage.GetUMat(AccessType.Read))
+            using (UMat uObservedImage = observedImage.GetUMat(AccessType.Read))
+            {
+                KAZE featureDetector = new KAZE();
+
+                Mat modelDescriptors = new Mat();
+                featureDetector.DetectAndCompute(uModelImage, null, modelKeyPoints, modelDescriptors, false);
+
+                Mat observedDescriptors = new Mat();
+                featureDetector.DetectAndCompute(uObservedImage, null, observedKeyPoints, observedDescriptors, false);
+
+                using (Emgu.CV.Flann.LinearIndexParams ip = new Emgu.CV.Flann.LinearIndexParams())
+                using (Emgu.CV.Flann.SearchParams sp = new SearchParams())
+                using (Emgu.CV.Features2D.DescriptorMatcher matcher = new FlannBasedMatcher(ip, sp))
+                {
+                    matcher.Add(modelDescriptors);
+
+                    matcher.KnnMatch(observedDescriptors, matches, k, null);
+                    mask = new Mat(matches.Size, 1, DepthType.Cv8U, 1);
+                    mask.SetTo(new MCvScalar(255));
+                    Features2DToolbox.VoteForUniqueness(matches, uniquenessThreshold, mask);
+
+                    int nonZeroCount = CvInvoke.CountNonZero(mask);
+                    if (nonZeroCount >= 4)
+                    {
+                        nonZeroCount = Features2DToolbox.VoteForSizeAndOrientation(modelKeyPoints, observedKeyPoints, matches, mask, 1.5, 20);
+                        if (nonZeroCount >= 4)
+                            homography = Features2DToolbox.GetHomographyMatrixFromMatchedFeatures(modelKeyPoints, observedKeyPoints, matches, mask, 2);
+                    }
+                }
+            }
+        }
+
+        public void Draw(Mat modelImage, Mat observedImage, string name)
+        {
+            Mat homography;
+            VectorOfKeyPoint modelKeyPoints;
+            VectorOfKeyPoint observedKeyPoints;
+            using (VectorOfVectorOfDMatch matches = new VectorOfVectorOfDMatch())
+            {
+                Mat mask;
+                FindMatch(modelImage, observedImage, out modelKeyPoints, out observedKeyPoints, matches, out mask, out homography);
+
+                if (homography != null)
+                {
+                    Rectangle rect = new Rectangle(Point.Empty, modelImage.Size);
+                    PointF[] pts = new PointF[]
+                    {
+                        new PointF(rect.Left, rect.Bottom),
+                        new PointF(rect.Right, rect.Bottom),
+                        new PointF(rect.Right, rect.Top),
+                        new PointF(rect.Left, rect.Top)
+                    };
+                    pts = CvInvoke.PerspectiveTransform(pts, homography);
+
+                    Point[] points = Array.ConvertAll<PointF, Point>(pts, Point.Round);
+
+                    frame.DrawPolyline(points, true, new Bgr(Color.Red), 2);
+                    label1.Text = name.Substring(0, name.LastIndexOf('.'));
+                }
+            }
         }
     }
 }
